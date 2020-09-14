8c8d64ab9709918b7155029cbbab59668ed6d189
Tekraj Joshi <tekrajjoshi07@gmail.com>
2020-09-14 09:08:33 +0530

    React routes setup

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -85,6 +85,9 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
